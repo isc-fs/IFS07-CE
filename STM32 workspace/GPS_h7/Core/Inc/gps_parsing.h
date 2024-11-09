17ccc2ab6cdffa450577afe34f5bd2cd47422706
Alberto Velasco Rodríguez <avelascorod@gmail.com>
2024-11-09 14:04:58 +0100

    GPS_h7

---
+++ STM32 workspace/GPS_h7/Core/Inc/gps_parsing.h
@@ -0,0 +1,33 @@
+/*
+ * gps_parsing.h
+ *
+ *  Created on: Nov 9, 2024
+ *      Author: avela
+ */
+
+#ifndef INC_GPS_PARSING_H_
+#define INC_GPS_PARSING_H_
+
+// Estructura para almacenar los datos GPGGA
+// Hay datos que representan floats, pero para parsearlos hace falta poner algo de una flag
+// que no consigo encontrar. Con esto lo guarda como un string y sí funciona.
+typedef struct {
+    char time[11]; 			// float
+    char latitude[10];		// float
+    char lat_direction;		// char
+    char longitude[11];		// float
+    char lon_direction;		// char
+    int fix_quality;		// int
+    int satellites;			// int
+    char hdop[8];			// float
+    char altitude[8];		// float
+    char altitude_units;	// char
+    char geoid_height[8];	// float
+    char geoid_units;		// char
+} GPGGA_Data;
+
+void process_gps_data(char* uart_buffer, GPGGA_Data *data);
+void parse_gpgga(char *gpgga_sentence, GPGGA_Data *data);
+
+
+#endif /* INC_GPS_PARSING_H_ */
